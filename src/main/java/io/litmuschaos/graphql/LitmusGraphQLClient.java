2ab23fec1a3d807f9bcacf728a8b38cd9f054f33
Jemin <jemin9812@gmail.com>
2025-02-02 22:01:58 +0900

    feat: add graphql exception handling logic
    Signed-off-by: Jemin <jemin9812@gmail.com>

--- src/main/java/io/litmuschaos/graphql/LitmusGraphQLClient.java
+++ src/main/java/io/litmuschaos/graphql/LitmusGraphQLClient.java
@@ -1,47 +1,50 @@
 package io.litmuschaos.graphql;
+import com.jayway.jsonpath.TypeRef;
 import com.netflix.graphql.dgs.client.GraphQLClient;
 import com.netflix.graphql.dgs.client.GraphQLResponse;
 import com.netflix.graphql.dgs.client.HttpResponse;
 import java.io.IOException;
-import java.util.Map;
 
+import io.litmuschaos.constants.HttpStatus;
+import io.litmuschaos.exception.LitmusApiException;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-
-import static io.litmuschaos.constants.RequestHeaders.BEARER;
+import static io.litmuschaos.constants.RequestHeaders.*;
 
 
 public class LitmusGraphQLClient {
 
-    public final GraphQLClient client;
+    private final GraphQLClient client;
 
     public LitmusGraphQLClient(OkHttpClient okHttpClient, String host, String token) {
         client = GraphQLClient.createCustom(host, ((url, headers, body) -> {
             Request request = new Request.Builder()
                 .url(url)
-                .addHeader("Authorization", BEARER + " " + token)
-                .post(RequestBody.create(body, MediaType.parse("application/json"))
+                .addHeader(AUTHORIZATION, BEARER + " " + token)
+                .post(RequestBody.create(body, MediaType.parse(APPLICATION_JSON))
             ).build();
 
             try (Response response = okHttpClient.newCall(request).execute()) {
-                if(!response.isSuccessful()) {
-                    throw new IOException("Unexpected code " + response);
-                }
-                return new HttpResponse(response.code(), response.body().string());
+                return new HttpResponse(HttpStatus.OK, response.body().string());
             }catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }));
     }
 
-    public GraphQLResponse query(String query, Map<String,Object> variables){
-        return client.executeQuery(query, variables);
+    public <T> T query(String query, String operationName, TypeRef<T> type) throws LitmusApiException {
+        GraphQLResponse response = client.executeQuery(query);
+        validateResponse(response);
+        return response.extractValueAsObject(operationName, type);
     }
 
-    public GraphQLResponse query(String query){
-        return client.executeQuery(query);
+    // If there are values in the Errors array of the Response, throw exception with first error message.
+    private void validateResponse(GraphQLResponse response) throws LitmusApiException {
+        if(response.hasErrors()){
+            throw new LitmusApiException(response.getErrors().get(0).getMessage());
+        }
     }
 }
